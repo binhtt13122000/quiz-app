b1664e3b1a0e2d6705132b2aa4979c05a7aaa9f4
BinhTT1312 <binhtt.13122000@gmail.com>
2021-02-20 15:15:38 +0700

    remove add redundant api

--- src/java/binhtt/controllers/SubmitController.java
+++ src/java/binhtt/controllers/SubmitController.java
@@ -16,7 +16,10 @@ import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import binhtt.dtos.UserDTO;
 import org.apache.log4j.Logger;
--- src/java/binhtt/daos/AnswerDAO.java
+++
@@ -1,6 +0,0 @@
-package binhtt.daos;
-
-import java.io.Serializable;
-
-public class AnswerDAO implements Serializable {
-}
